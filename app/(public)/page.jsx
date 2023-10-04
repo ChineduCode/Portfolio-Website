5b2377f6b9485d68b8b909d9004f7f302333c1f9
Chinedu <chinedunnaemeka2@gmail.com>
2023-10-05 00:36:14 +0100

    projects component added, index page modified

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -2,20 +2,8 @@ import WorkStats from '../../components/WorkStats'
 import HireMe from '@/components/Hire_Me'
 import DownloadCV from '../../components/DownloadCV'
 import Testimonials from '../../components/Testimonial'
+import Projects from '@/components/Projects'
 import Link from 'next/link'
-import { NextResponse } from 'next/server'
-
-async function getPortfolio(){
-  const res = await fetch(`http://localhost:3000/api/portfolio`, {
-    next: { revalidate : 10 }
-  })
-  
-  if(!res.ok){
-    return NextResponse.json('Failed to fetch portfolios')
-  }
-
-  return res.json();
-}
 
 export default async function Home(){
   
@@ -49,10 +37,6 @@ export default async function Home(){
       percentage: '94%'
     },
   ]
-
-  //fetch portfolio
-  const portfolios = await getPortfolio()
-  const minimalizedPortfolio = portfolios.slice(0,2)
 
   return (
     <section className='home'>
@@ -103,23 +87,7 @@ export default async function Home(){
           </div>
         </div>
 
-        <div className="portfolios">
-          {minimalizedPortfolio.map((portfolio, index) => 
-            <div className='content' key={index}>
-              <div className="project_design">
-                <div className="project_screenshot"> <img src={portfolio.image} alt='image' /> </div>
-              </div>
-              <div className="title_tools">
-                <h3 className='title'> {portfolio.name} </h3>
-                <div className="tools"> {portfolio.tools} </div>
-              </div>
-              <div className='links'>
-                <Link href={portfolio.live_link} target='_blank' className='live'><button>View Live</button></Link>
-                <Link href={portfolio.github_link} target='_blank' className='github'><button>View Code</button></Link>
-              </div>
-            </div> 
-          )}
-        </div>
+        <Projects />
 
         <Link href={'/portfolio'} className='view_more'>View more ...</Link>
       </section>
---
+++ components/Projects.js
@@ -0,0 +1,35 @@
+import Link from "next/link"
+
+async function fetchProjects(){
+    const res = await fetch('http://localhost:3000/api/portfolio', {
+        next: {revalidate: 10}
+    })
+    const projects = await res.json()
+    return projects.slice(0, 2);
+}
+
+export default async function Projects(){
+    //fetch portfolio
+    const projects = await fetchProjects()
+
+    return(
+        <div className="portfolios">
+            {projects.map((portfolio, index) => 
+                <div className='content' key={index}>
+                    <div className="project_design">
+                        <div className="project_screenshot"> <img src={portfolio.image} alt='image' /> </div>
+                    </div>
+                    <div className="title_tools">
+                        <h3 className='title'> {portfolio.name} </h3>
+                        <div className="tools"> {portfolio.tools} </div>
+                    </div>
+                    <div className='links'>
+                        <Link href={portfolio.live_link} target='_blank' className='live'><button>View Live</button></Link>
+                        <Link href={portfolio.github_link} target='_blank' className='github'><button>View Code</button></Link>
+                    </div>
+                </div> 
+            )}
+        </div>
+    )
+
+}
