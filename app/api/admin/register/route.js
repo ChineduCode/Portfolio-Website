4db7a6f39a5c1f5d69e419dafce93e1d001c16bd
Chinedu <chinedunnaemeka2@gmail.com>
2023-10-12 04:10:07 +0100

    cookie added for authentication

--- app/api/admin/register/route.js
+++ app/api/admin/register/route.js
@@ -2,12 +2,14 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/connectDB";
 import Admin from "@/Models/admin";
 import bcrypt from 'bcryptjs'
+import { serialize } from "cookie";
 import { generateToken } from "@/lib/auth";
 
 export async function POST(request){
 
     const res = await request.json()
     const {username, email, password, confirmPassword} = res
+    const MAX_AGE = process.env.MAX_AGE
 
     if(!username|| !email || !password){
         return NextResponse.json({'msg': 'Please fill all fields'})
@@ -28,7 +30,9 @@ export async function POST(request){
         //Check if email already exist
         const adminExists = await Admin.findOne({email})
         if(adminExists){
-            throw new Error('Admin with the email already exist')
+            return new Response('Admin with the email already exist', {
+                status: 404
+            })
         }
         
         //Hash Password
@@ -43,7 +47,14 @@ export async function POST(request){
 
         await admin.save()
 
-        return NextResponse.json(admin)
+        //Generate token
+        const token = generateToken(admin)
+        const serialized = serialize('OutSiteJwt', token, {
+            httpOnly: true,
+            maxAge: MAX_AGE
+        })
+
+        return new Response(admin, {status: 200})
         
     } catch (error) {
         throw new Error(error)
