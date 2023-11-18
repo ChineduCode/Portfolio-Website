bf8ab2d83817f0f2024622317ea774a0e79e67fc
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-18 03:21:34 +0100

    register api modified

--- app/api/admin/register/route.js
+++ app/api/admin/register/route.js
@@ -2,14 +2,11 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/connectDB";
 import Admin from "@/Models/admin";
 import bcrypt from 'bcryptjs'
-import { serialize } from "cookie";
-import { generateToken } from "@/lib/auth";
 
 export async function POST(request){
 
     const res = await request.json()
     const {username, email, password, confirmPassword} = res
-    const MAX_AGE = process.env.MAX_AGE
 
     if(!username|| !email || !password){
         return NextResponse.json({'msg': 'Please fill all fields'})
@@ -47,16 +44,8 @@ export async function POST(request){
 
         await admin.save()
 
-        //Generate token
-        const token = generateToken(admin)
-        const serialized = serialize('OutSiteJwt', token, {
-            httpOnly: true,
-            maxAge: 60 * 60 * 24 * 30
-        })
-
         return new Response(admin, {
             status: 200,
-            headers: {'Set-Cookie': serialized}
         })
         
     } catch (error) {
