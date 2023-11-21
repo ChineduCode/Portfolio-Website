baa4ae200e6a6af34b51f4d04173d1c844e772a4
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-21 01:18:57 +0100

    loginpage error checked

--- app/(admin)/admin/login/page.jsx
+++ app/(admin)/admin/login/page.jsx
@@ -1,11 +1,11 @@
 import Login from "@/components/Login"
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
+//import { getServerSession } from "next-auth"
+//import { redirect } from "next/navigation"
  
 export default async function LoginPage(){
-    const session = await getServerSession()
+    // const session = await getServerSession()
 
-    if(session) redirect('/admin/dashboard')
+    // if(session) redirect('/admin/dashboard')
 
     return(
         <Login />
